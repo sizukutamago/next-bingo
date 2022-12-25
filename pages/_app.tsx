dad9b5897de1f0f4093e95476d316a43049e6681
sizukutamago <sizukutamago@gmail.com>
2022-12-25 16:29:11 +0900

    add chakra ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const theme = extendTheme({
+    styles: {
+      global: {
+        body: {
+          backgroundColor: '#defcff',
+        },
+      },
+      html: {
+        height: '100%',
+        width: '100%',
+      },
+    },
+  });
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
 }
